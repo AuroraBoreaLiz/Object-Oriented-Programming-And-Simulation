20bc850deaba10f204d0f4fd25a87f39622e0f2a
AuroraBoreaLiz <lizmcquaidmartin@gmail.com>
2023-03-29 18:21:43 -0400

    Update sketch.js
    particle goes up

--- sketch.js
+++ sketch.js
@@ -9,6 +9,7 @@ function draw() {
   background(0);
   
   for (let i = 0; i <particles.length; i++){
+    particles[i].update();
     particles[i].show();
   }
 
@@ -21,8 +22,15 @@ class Particle {
     //particles start at the bottom
     this.x = 300;
     this.y = 380;
+    this.vx = random(-1,1);
+    this.vy = random(-5,-1);
+    
   }
   
+  update(){
+    this.x += this.vx;
+    this.y += this.vy;
+  }
   show () {
     stroke(255);
     fill (255,10);
