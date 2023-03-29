f25b9a3619dd35540e60a2bf3823c8b67b7531a6
AuroraBoreaLiz <lizmcquaidmartin@gmail.com>
2023-03-29 18:17:15 -0400

    Update sketch.js
    Following tutorial https://thecodingtrain.com/challenges/78-simple-particle-system

--- sketch.js
+++ sketch.js
@@ -1,49 +1,27 @@
-let bugs = []; // Declare object
-
+let p;
 function setup() {
-  createCanvas(710, 400);
-  // Create more bugs
-  for (let i = 0; i < 3; i++) {
-    bugs[i]= new Jitter();
-  }
+  createCanvas(600, 400);
+  p = new Particle();
 }
 
 function draw() {
-  background(50, 89, 100);
-  for (let i = 0; i < bugs.length; i++) {
-    bugs[i].move();
-    bugs[i].display();
-  }
-
+  background(0);
+  
+  p.show();
 }
 
-// Jitter class
-class Jitter {
-  constructor() {
-    this.x = width/2;
-    this.y = height/2;
-    this.diameter = random(10, 30);
-    //If I adjust this number the random horizontal distance will go up in the move()
-    this.speed = 5;
+class Particle {
+  
+  
+  constructor(){
+    //particles start at the bottom
+    this.x = 300;
+    this.y = 380;
   }
-
-  move() {
-
-    this.x += random(-this.speed, this.speed);
-    //if you adjust this number to be positive the bug will go down!
-    //if you adjust this number to be negative the bug will go up!
-    //using a larger number makes the bug go faster.
-    this.y += -4;
- 
-
-    if (this.y < 0) {
-      this.y = height/1.5;
-    }
-
-  }
-
-  display() {
-    ellipse(this.x, this.y, this.diameter, this.diameter);
+  
+  show () {
+    stroke(255);
+    fill (255,10);
+    ellipse(this.x, this.y, 16);
   }
 }
-
